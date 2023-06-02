3eb20973679ecf51e2543b5d5da5b3d7322f3473
Joshua Avalon <git@avalon.sh>
2023-06-02 20:33:20 +0800

    feat: Update

--- docker/root/app/start.sh
+++ docker/root/app/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd /app;
-exec s6-setuidgid node npm run --cache /home/node/.npm db:deploy;
+npm run --cache /home/node/.npm db:deploy;
 
 if [ "$FILELIA__LOG_TYPE" == "json" ]; then
   exec s6-setuidgid node npm run --cache /home/node/.npm start:json;
