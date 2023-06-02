89bbf8664c4df1c7e9f0c305b568d9e8f3f1cc10
Joshua Avalon <git@avalon.sh>
2023-06-02 20:04:51 +0800

    feat: Update

--- packages/next/next.config.js
+++ packages/next/next.config.js
@@ -1,10 +1,11 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: false
-});
+// const withBundleAnalyzer = require("@next/bundle-analyzer")({
+//   enabled: false
+// });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compress: true
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
+// module.exports = withBundleAnalyzer(nextConfig);
+module.exports = nextConfig;
